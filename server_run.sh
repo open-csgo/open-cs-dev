8d0374de3733ad34f0cf7e9882adcb83417408de
AFCMS <afcm.contact@gmail.com>
2022-01-13 18:16:22 +0100

    fix server running script

--- server_run.sh
+++ server_run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/zsh
 
-if [[ -f "./server_build/minetest/games/open-cs" ]] then
+# https://unix.stackexchange.com/questions/363143/output-in-color-bash
+
+if [[ -d "./server_build/minetest/games/open-cs" ]] then
+	echo "\e[1;32mLaunching the server\e[1;0m"
     ./server_build/minetest/bin/minetestserver --gameid open-cs --world ./server_world --config ./server_config.conf
 else
-	echo "You have not linked open-cs to ./server_build/minetest/games/open-cs"
+	echo "\e[1;31mYou have not linked open-cs to ./server_build/minetest/games/open-cs\e[1;0m"
 	exit 1
 fi
